b1c0466704915367380e4a81939fb6c50d41566f
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2021-04-19 17:37:20 -0400

    played a little with material-ui. pretty cool, if i do say so myself :100:

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,15 +1,65 @@
 import React from "react";
-import { Navbar, Nav, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button, IconButton, List, ListItem, Drawer, ListItemIcon, ListItemText } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Assessment, BurstMode, Email, CalendarToday } from '@material-ui/icons';
 
-const Header = (props) => {
-    return (
-        <Navbar sticky="top" className="navbar gradient">
-			<Nav className="ml-auto nav-text">
-				<Nav.Link as={Link} to="/"> Link</Nav.Link>
-			</Nav>
-		</Navbar>
-    )
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
+
+export default function Header(props) {
+	const classes = useStyles();
+	const [open, setOpen] = React.useState(false);
+	const [count, setCount] = React.useState(0);
+	const toggleDrawer = () => {
+		setOpen(!open);
+	};
+	const switchIcon = (i) => {
+		switch(i) {
+			case 0:
+				return <Assessment/> 
+			case 1:
+				return <BurstMode/> 
+			case 2:
+				return <Email/>
+			case 3:
+				return <CalendarToday/>
+		}
+	}
+	const list = <List>
+		{['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4'].map((text, index) => (
+			<ListItem button key={text}>
+				<ListItemIcon> {switchIcon(index) } </ListItemIcon>
+				<ListItemText primary={text} />
+			</ListItem>
+		))}
+	</List>
+	return (<>
+		<AppBar position="static">
+			<Toolbar>
+				<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer}>
+					<MenuIcon />
+				</IconButton>
+				<Typography variant="h6" className={classes.title}>
+					Home
+          		</Typography>
+				<Button color="inherit" onClick={()=>setCount(count+1)}>Click me</Button>
+				{count}
+			</Toolbar>
+		</AppBar>
+		<Drawer anchor={"left"} open={open} onClose={toggleDrawer}>
+			{list}
+		</Drawer>
+
+	</>)
 }
-
-export default Header;
